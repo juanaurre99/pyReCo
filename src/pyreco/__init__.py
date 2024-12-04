60259c42de687eaa4fac1eb105f780155203e4d0
klara_susanna <disson@campus.tu-berlin.de>
2024-12-04 11:04:56 +0100

    Add src code and README
    Signed-off-by: klara_susanna <disson@campus.tu-berlin.de>

--- src/pyreco/__init__.py
+++ src/pyreco/__init__.py
@@ -1,16 +1,6 @@
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
-
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
+#
+# from . import layers
+# from .layers import InputLayer, ReservoirLayer, ReadoutLayer
+#
+# from . import custom_models
+# from .custom_models import RC
