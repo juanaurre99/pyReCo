5f2d5ea946275d9ffae2b0b9d089d4d5c9181824
mertstend <merten.stender@tu-berlin.de>
2025-02-05 21:48:58 +0100

    further additions to the initializer
    Signed-off-by: mertstend <merten.stender@tu-berlin.de>

--- src/pruning.py
+++ src/pruning.py
@@ -3,6 +3,7 @@ Capabilities to prune an existing RC model, i.e. try to cut reservoir nodes and 
 performance while reducing the reservoir size
 """
 
+import numpy as np
 from pyreco.custom_models import RC
 
 
@@ -16,7 +17,24 @@ class NetworkPruner:
         min_num_nodes: int = None,
         patience: int = None,
     ):
-        # initializer for the pruning class.
+        """
+        Initializer for the pruning class.
+
+        Parameters:
+        - target_score (float): The test set score that the user aims at. Pruning stops
+        once this score is reached.
+
+        - stop_at_minimum (bool): Whether to stop at the local minimum of the test set
+        score. When set to False, pruning continues until the minimal number of nodes
+        in <min_num_nodes>.
+
+        - min_num_nodes (int): Stop pruning when arriving at this number of nodes.
+        Conflicts if stop_at_minimum is set to True but also a min_num_nodes is given.
+
+        - patience (int): We allow a patience, i.e. keep pruning after we reached a
+        (local) minimum of the test set score. Depends on the size of the original
+        reservoir network, defaults to 10% of initial reservoir nodes.
+        """
 
         # Sanity checks for the input parameter types and values
         if target_score is not None and not isinstance(target_score, float):
@@ -46,6 +64,36 @@ class NetworkPruner:
 
     def prune(self, model: RC, data_train: tuple, data_val: tuple):
         # prune a given model by removing nodes.
+
+        # Sanity checks for the input parameter types and values
+        if not isinstance(model, RC):
+            raise TypeError("model must be an instance of RC")
+
+        if not isinstance(data_train, tuple) or not isinstance(data_val, tuple):
+            raise TypeError("data_train and data_val must be tuples")
+
+        if len(data_train) != 2 or len(data_val) != 2:
+            raise ValueError("data_train and data_val must have 2 elements each")
+
+        for idx, elem in enumerate(data_train):
+            if not isinstance(elem, list) or not isinstance(elem, np.ndarray):
+                raise TypeError(f"data_train[{idx}] must be a list or numpy array")
+
+        for idx, elem in enumerate(data_val):
+            if not isinstance(elem, list) or not isinstance(elem, np.ndarray):
+                raise TypeError(f"data_val[{idx}] must be a list or numpy array")
+
+        if len(data_train[0]) != len(data_train[1]):
+            raise ValueError(
+                "data_train[0] and data_train[1] must have the same length, "
+                "i.e. same number of samples"
+            )
+
+        if len(data_val[0]) != len(data_val[1]):
+            raise ValueError(
+                "data_val[0] and data_val[1] must have the same length, "
+                "i.e. same number of samples"
+            )
 
         N = model.reservoir_layer.nodes  # number of initial reservoir nodes
 
